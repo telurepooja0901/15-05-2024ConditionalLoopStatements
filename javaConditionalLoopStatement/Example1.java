98f69f4c303d717b7f0ee0df6000850457704363
telurepooja0901 <telurepooja0901@gmail.com>
2024-06-17 16:04:32 +0530

    first commit

---
+++ javaConditionalLoopStatement/Example1.java
@@ -0,0 +1,27 @@
+package com.javaConditionalLoopStatement;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		
+		String names[] = {"pooja", "rutuja", "manisha", "pallavi", "shubham"};
+		
+		System.out.println(names[0]);
+		System.out.println(names[1]);
+		System.out.println(names[2]);
+		System.out.println(names[3]);
+		System.out.println(names[4]);
+		
+		System.out.println();
+
+		
+		
+		for(int i=0; i<=4; i++)
+		{
+			System.out.println(names[i]);
+
+		}		
+	}
+
+}
